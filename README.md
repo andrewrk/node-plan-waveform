090e3712880a8d9415f56d9f0e7e7d294626c955
Andrew Kelley <superjoe30@gmail.com>
2012-12-05 14:40:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+See [node-plan](https://github.com/superjoe30/node-plan)
+
+### input
+
+  * `tempPath` - an audio file to generate a waveform for
+  * `makeTemp` - a function which generates a tempfile and accepts a suffix
+
+### output
+
+  * `tempPath` - a .png file which is a waveform of the audio
+
+### options
+
+  * `width`
+  * `height`
+  * `colorBg` - formatted like 'ffaabb00'
+  * `colorCenter`
+  * `colorOuter`
+
+### exports
+
+none
---
+++ index.js
@@ -0,0 +1,26 @@
+var waveform = require('waveform');
+
+module.exports = {
+  cpuBound: true,
+  start: function(done) {
+    var self = this;
+    var tempPngFile = self.context.makeTempFile({suffix: '.png'});
+    var waveformOptions = {
+      width: self.options.width,
+      height: self.options.height,
+      'color-bg': self.options.colorBg,
+      'color-center': self.options.colorCenter,
+      'color-outer': self.options.colorOuter,
+    };
+    tempPath = self.context.tempPath;
+    delete self.context.tempPath;
+    waveform(tempPath, tempPngFile, waveformOptions, function(err) {
+      if (err) {
+        done(err);
+      } else {
+        self.context.tempPath = tempPngFile;
+        done();
+      }
+    });
+  }
+};
